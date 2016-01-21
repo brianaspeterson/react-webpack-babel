01da1850ae0e68ef1684533ac9e7b88662f2de64
[Peterson, Briana] <briana.s.peterson@jpmchase.com>
2016-01-21 12:47:03 -0500

    react - webpack initial setup

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Time to code!");
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var HelloComponent = React.createClass({
+	render: function(){
+			return (
+				<div>
+					hello
+				</div>
+			);
+		}
+	});
+
+ReactDOM.render(<HelloComponent/>, document.getElementById('content'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
@@ -7,5 +8,17 @@ module.exports = {
   output : {
     path : path.join(__dirname, 'dist'),
     filename : "[name].js"
-  }
+  },
+  module: {
+	  loaders: [
+	    {
+	      test: /\.jsx?$/,
+	      exclude: /(node_modules|bower_components)/,
+	      loader: 'babel', // 'babel-loader' is also a legal name to reference
+	      query: {
+	        presets: ['react', 'es2015']
+	      }
+	    }
+	  ]
+	}
 };
